e5516aba8650d12b85a8b75173d7fd0451dbe582
Yuri Skurikhin <yunixon@gmail.com>
2014-07-25 04:01:16 +0000

    Change rspec and travis files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-  require 'rspec/autorun'
   require 'webmock/rspec'
 
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
@@ -26,7 +25,7 @@ Spork.prefork do
       DatabaseCleaner.clean_with(:deletion)
     end
 
-    config.before(:each) do
+    config.before(:each) do |example|
       DatabaseCleaner.strategy = example.metadata[:type] == :feature ? :deletion : :transaction
       DatabaseCleaner.start
     end
