a4c7594a3c27a3268f3c49de03a10037e0d5b1e2
Yuri Skurikhin <yunixon@gmail.com>
2014-07-25 04:20:24 +0000

    Add heroku link and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Event.create(name: "Germany Bundesliga: FC Bayern München - VfL Wolfsburg",
+  event_type: 1, first_side: "FC Bayern München", second_side: "VfL Wolfsburg",
+  datetime_start: "2014-11-17 10:00:00 UTC", complete: false, count: "0:0",
+  complete_type: 0)
