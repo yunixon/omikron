a9f5091bf51e7a3c9730bfce3b2d21860f07a9c6
Yuri Skurikhin <yunixon@gmail.com>
2014-07-27 17:49:35 +0000

    Factory Faker Internet password min_length 8 for devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,16 +3,16 @@
 FactoryGirl.define do
   factory :user do
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password { Faker::Internet.password(min_length = 8) }
     role { :user }
     factory :first_user do
       email { Faker::Internet.email }
-      password { Faker::Internet.password }
+      password { Faker::Internet.password(min_length = 8) }
       role { :user }
     end
     factory :second_user do
       email { Faker::Internet.email }
-      password { Faker::Internet.password }
+      password { Faker::Internet.password(min_length = 8) }
       role { :admin }
     end
   end
