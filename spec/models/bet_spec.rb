a3ec2dfaefd00b33fd852a0176ad5bb9a6220c66
meowcode <amb3rus@gmail.com>
2014-07-27 15:11:06 +0400

    Added factory and spec for bets

--- spec/models/bet_spec.rb
+++ spec/models/bet_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Bet, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:bet)).to be_valid
+  end
+
+  it { is_expected.to belong_to(:user).class_name('User') }
+  it { is_expected.to belong_to(:event).class_name('Event') }
+
 end
