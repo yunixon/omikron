281cd9835e78fc32c891fd64136182cc171bb01d
Yuri Skurikhin <yunixon@gmail.com>
2014-07-27 16:59:17 +0000

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726170814) do
+ActiveRecord::Schema.define(version: 20140727180224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,20 @@ ActiveRecord::Schema.define(version: 20140726170814) do
     t.integer  "side_bet"
     t.boolean  "complete"
     t.integer  "complete_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "complete_types", force: true do |t|
+    t.string   "result"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "event_types", force: true do |t|
+    t.string   "name"
+    t.text     "descrition"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
