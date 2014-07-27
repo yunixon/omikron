929ffc7d1a29efe33b55b1a747f231511f67c193
sozinovss <Sergey@Sergeys-iMac.local>
2014-07-27 13:02:49 -0700

    edit fabric complete_type and event_type, link to event

--- spec/models/complete_type_spec.rb
+++ spec/models/complete_type_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe CompleteType, :type => :model do
    it "has a valid factory" do
   	expect(FactoryGirl.build(:complete_type)).to be_valid
   end
-
-  it { expect have_many :events}
+  
+  it { is_expected.to have_many(:events) }
 end
