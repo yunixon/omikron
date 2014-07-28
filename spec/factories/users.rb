a404410f4cdcb56850a8b2593221718c15826d67
Yuri Skurikhin <yunixon@gmail.com>
2014-07-28 08:39:10 +0000

    Repair specs and factories for Users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,15 +5,5 @@ FactoryGirl.define do
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length = 8) }
     role { :user }
-    factory :first_user do
-      email { Faker::Internet.email }
-      password { Faker::Internet.password(min_length = 8) }
-      role { :user }
-    end
-    factory :second_user do
-      email { Faker::Internet.email }
-      password { Faker::Internet.password(min_length = 8) }
-      role { :admin }
-    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-
-  before :all do
-    @first_user = FactoryGirl.create :first_user
-    @second_user = FactoryGirl.create :second_user
-  end
-  
+ 
   it "has a valid factory" do
     expect(FactoryGirl.build(:user)).to be_valid
   end
