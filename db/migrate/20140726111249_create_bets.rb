4448757195c628a2b4c8dbc6ecc672c10fd87036
Roman <amb3rus@gmail.com>
2014-07-28 10:51:52 +0400

    Update 20140726111249_create_bets.rb
    Fixed fields for user_id and event_id

--- db/migrate/20140726111249_create_bets.rb
+++ db/migrate/20140726111249_create_bets.rb
@@ -1,8 +1,8 @@
 class CreateBets < ActiveRecord::Migration
   def change
     create_table :bets do |t|
-      t.integer :user_id
-      t.integer :event_id
+      t.belongs_to :user
+      t.belongs_to :event
       t.decimal :sum
       t.integer :side_bet
       t.boolean :complete
