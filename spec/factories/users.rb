0a6ba8d1999bab6847994e36fd899c6a82ca7bfe
Yuri Skurikhin <yunixon@gmail.com>
2014-07-29 17:07:56 +0000

    Add admin factory and spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,9 @@ FactoryGirl.define do
     password { Faker::Internet.password(min_length = 8) }
     role { :user }
   end
+  factory :admin do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(min_length = 8) }
+    role { :admin }
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe User, :type => :model do
     expect(FactoryGirl.build(:user)).to be_valid
   end
   
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:admin)).to be_valid
+  end
+  
   it { is_expected.to have_many(:bets) }
   
 end
