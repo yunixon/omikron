246e26a97d50cc893b7f720461fe4ee88cfc2173
Yuri Skurikhin <yunixon@gmail.com>
2014-07-30 09:44:38 +0000

    Devise scope for :user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  
+  devise_scope :user do
+    get "sign_in", to: "devise/sessions#new"
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
