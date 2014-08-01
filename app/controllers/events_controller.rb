a516acde48b54897b42a67352d6d2028af25eb09
Yuri Skurikhin <yunixon@gmail.com>
2014-08-01 04:01:30 +0000

    Events controller CRUD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,56 @@
 class EventsController < ApplicationController
+  
+  before_action :find_event, only: [:show, :edit, :update]
+  
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    event_setup(@event)
+    if @event.save
+      flash[:success] = "Event added!"
+      redirect_to @event
+    else
+      render 'new'
+    end
+    
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update_attributes(event_params)
+      flash[:success] = "Event updated"
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Event.find(params[:id]).destroy
+    flash[:success] = "Event destroyed."
+    redirect_to events_url
+  end
+  
+  private
+  
+  def find_event
+    @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:name, :event_type_id, :first_side, :second_side, :datetime_start)
+  end
+  
 end
