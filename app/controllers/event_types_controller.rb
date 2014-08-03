420cc727878ae4237d7727be25dad7b25c21f134
meowcode <amb3rus@gmail.com>
2014-08-03 20:50:39 +0400

    Changed event_types controller and added event_types views

--- app/controllers/event_types_controller.rb
+++ app/controllers/event_types_controller.rb
@@ -1,2 +1,48 @@
 class EventTypesController < ApplicationController
+  load_and_authorize_resource param_method: :event_type_params
+  before_action :find_event_type, only: [:show, :edit, :update]
+  
+  def index
+  end
+
+  def new
+    @event_type = EventType.new
+  end
+
+  def create
+    @event_type = EventType.new(event_type_params)
+    if @event_type.save
+      flash[:success] = "Category successfully added."
+      redirect_to event_types_path
+    else
+      render 'new'
+    end
+    
+  end
+
+  def update
+    if @event_type.update_attributes(event_type_params)
+      flash[:success] = "Category updated."
+      redirect_to @event_type
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    EventType.find(params[:id]).destroy
+    flash[:success] = "Category destroyed."
+    redirect_to event_types_path
+  end
+  
+  private
+  
+  def find_event_type
+    @event_type = EventType.find(params[:id])
+  end
+
+  def event_type_params
+    params.require(:event_type).permit(:name, :event_type_type_id, :description)
+  end
+  
 end
