c338acba90b150e5de5203064b9167c3fecd62b7
Roman <amb3rus@gmail.com>
2014-08-03 21:58:15 +0400

    Update event_types_controller.rb

--- app/controllers/event_types_controller.rb
+++ app/controllers/event_types_controller.rb
@@ -3,6 +3,7 @@ class EventTypesController < ApplicationController
   before_action :find_event_type, only: [:show, :edit, :update]
   
   def index
+    @event_types = EventType.all
   end
 
   def new
