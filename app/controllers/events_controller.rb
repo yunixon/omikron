7f9c9e874ab6ed57d28c936c277aad87cd8ed5db
meowcode <amb3rus@gmail.com>
2014-08-04 10:22:27 +0400

    event_types controller, spec and pagination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   
   def index
     @search = Event.search(params[:q])
-    @events = @search.result(distinct: true)
+    @events = @search.result(distinct: true).order(:datetime_start).page(params[:page]).per(12)
   end
 
   def show
