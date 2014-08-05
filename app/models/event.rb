7427530c8caffe52d142f9dc887a65b9d77db437
Yuri Skurikhin <yunixon@gmail.com>
2014-08-05 04:49:44 +0000

    Add some validations to Event and User

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,14 @@
 class Event < ActiveRecord::Base
   
-  has_many :bets
-  
+  has_many   :bets
   belongs_to :event_type
   belongs_to :complete_type
   
   accepts_nested_attributes_for :bets
   
+  validates :name,             presence: true, length: { in: 3..120 }
+  validates :first_side,       presence: true, length: { in: 3..80 }
+  validates :second_side,      presence: true, length: { in: 3..80 }
+  validates :event_type_id,    presence: true
+  validates :datetime_start,   presence: true
 end
