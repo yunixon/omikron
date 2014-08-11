65a821c897704962eb0d42e39c33887d33ab507e
Yuri Skurikhin <yunixon@gmail.com>
2014-08-11 06:45:27 +0000

    Rename dev server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,8 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'http://cbets-91815.apne1.nitrousbox.com:3000' }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: '0.0.0.0:3000' }
   config.action_mailer.perform_deliveries = false
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.delivery_method = :smtp
