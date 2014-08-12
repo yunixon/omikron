2e4c5a745d3d434cd0a771c250e9fcd87ca0b639
Yuri Skurikhin <yunixon@gmail.com>
2014-08-12 11:51:38 +0000

    Event refactoring

--- app/models/event.rb
+++ app/models/event.rb
@@ -23,43 +23,47 @@ class Event < ActiveRecord::Base
   protected
   
   def play_bets
-    # Высчитать сумму проигравших ставок и забрать от них 3% себе :)
-    all_bets_sum = self.bets.sum("sum") # Сумма всех ставок
-    all_bets_lose_sum = self.bets.where.not("side_bet = ?", self.complete_type).sum("sum")
-    all_bets_win_sum = all_bets_sum - all_bets_lose_sum
-    fee = 0.03*all_bets_lose_sum #3% fee
-    if self.bets.any?
-      self.bets.each do |bet|
-        if (self.complete_type.result == bet.side_bet && (not bet.complete))
-          bet.update(complete: true, complete_type: :win)
-          win_amount = bet.sum + bet.sum * (all_bets_lose_sum - fee) / all_bets_win_sum
-          @transaction = Transaction.new(user: bet.user, bet_id: bet.id,
-            amount: bet.sum, t_type: :win_bet)
-          if @transaction.save
-            @transaction.update(complete: true)
-          else
-            errors.add(:msg, "Error: transaction not saved")
-          end
-        elsif (self.complete_type.result != bet.side_bet &&
-          self.complete_type.result != "unplayed" && (not bet.complete))
-          bet.update(complete: true, complete_type: :lose)
-        elsif (self.complete_type.result == "unplayed")
-          bet.update(complete: false, complete_type: :unplayed)
-          # Возвратные транзакции
-          @transaction = Transaction.new(user: bet.user, bet_id: bet.id,
-            amount: bet.sum, t_type: :set_bet)
-          if @transaction.save
-            @transaction.update(complete: true)
-          else
-            errors.add(:msg, "Error: transaction not saved")
-          end
-        end
+    self.bets.each do |bet|
+      if self.complete_type.result == bet.side_bet && !bet.complete
+        user_win(bet)
+      elsif self.complete_type.result != bet.side_bet &&
+        user_lose(bet)
+      elsif (self.complete_type.result == "unplayed")
+        user_return(bet)
       end
     end
   end
   
+  def user_win(bet)
+    bet.update(complete: true, complete_type: :win)
+    @transaction = Transaction.new(user: bet.user, bet_id: bet.id,
+      amount: bet.sum, t_type: :win_bet)
+    if @transaction.save
+      @transaction.update(complete: true)
+    else
+      errors.add(:msg, "Error: transaction not saved")
+    end
+  end
+  
+  def user_lose(bet)
+    self.complete_type.result != "unplayed" && !bet.complete
+    bet.update(complete: true, complete_type: :lose)
+  end
+  
+  def user_return(bet)
+    bet.update(complete: false, complete_type: :unplayed)
+    # Возвратные транзакции
+    @transaction = Transaction.new(user: bet.user, bet_id: bet.id,
+      amount: bet.sum, t_type: :set_bet)
+    if @transaction.save
+      @transaction.update(complete: true)
+    else
+      errors.add(:msg, "Error: transaction not saved")
+    end
+  end
+  
   def self.start_events
-    events = Event.all.where("datetime_start <= ? and complete = ?", Time.now, false)
+    events = Event.where("datetime_start <= ? and complete = ?", Time.now, false)
     events.each do |event|
       #Посылаем письмо админу о стартанувших евентах
       Notifier.start_event_mail(event).deliver
