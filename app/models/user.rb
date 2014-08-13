aba1bfe99514821418a16def5c5268b4864090be
Yuri Skurikhin <yunixon@gmail.com>
2014-08-13 04:34:23 +0000

    email maximum validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   has_many :transactions, inverse_of: :user, dependent: :destroy
   
   validates :balance, numericality: { greater_than_or_equal_to: 0.0, less_than: 999999.99 }
+  validates :email, length: {minimum: 4, maximum: 60}
   
   accepts_nested_attributes_for :bets, allow_destroy: true
   
